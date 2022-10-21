e078c83a87ad74e9671cb44117a888c806116a45
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-21 14:07:01 +0200

    Add project files.

---
+++ uppgift 4.23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift_4._26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (siffra == 9)
+                {
+                    Console.Write("0");
+                }
+                else
+                {
+                    Console.Write(siffra + 1);
+                }
+            }
+        }
+    }
+}
